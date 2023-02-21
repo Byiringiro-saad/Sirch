3e0d697b075ab806b6bedcec6a35437310c01e59
Byiringiro Saad <byiringirosaad@gmail.com>
2023-02-21 08:40:18 +0200

    nav and footer

---
+++ src/components/footer.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+import styled from "styled-components";
+
+// eslint-disable-next-line react/function-component-definition
+const Footer = () => (
+  <Container>
+    <div className="left">
+      <a href="https://www.linkedin.com/company/43178146" rel="noreferrer" target="_blank">
+        Linkedin
+      </a>
+      <a
+        href="https://www.facebook.com/profile.php?id=100089316559376&mibextid=ZbWKwL"
+        rel="noreferrer"
+        target="_blank"
+      >
+        Facebook
+      </a>
+      <a href="https://twitter.com/SirchTheWeb" rel="noreferrer" target="_blank">
+        Twitter
+      </a>
+      <a href="https://www.tiktok.com/@sirch.it?_t=8ZFLQTtiPM6&_r=1" rel="noreferrer" target="_blank">
+        TikTok
+      </a>
+    </div>
+    <div className="right">
+      <p>Terms and Conditions</p>
+      <p>Privacy policy</p>
+      <p>@Copyright 2023 Sirch</p>
+    </div>
+  </Container>
+);
+
+const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 30px;
+  position: fixed;
+  bottom: 0;
+  left: 0;
+
+  .left {
+    width: auto;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    a {
+      margin: 0 30px 0 0;
+      color: var(--white);
+      text-decoration: none;
+    }
+  }
+
+  .right {
+    width: auto;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    p {
+      color: var(--white);
+      margin: 0 0 0 30px;
+    }
+  }
+`;
+
+export default Footer;
---
+++ src/components/nav.jsx
@@ -0,0 +1,76 @@
+import React from "react";
+import styled from "styled-components";
+
+function Nav() {
+  return (
+    <Container>
+      <div className="logo">
+        <img src="/logo.png" alt="Sirch" />
+      </div>
+      <div className="navigation">
+        <a href="https://sirch.cash/" rel="noreferrer" target="_blank" className="link">
+          Download for Chrome
+        </a>
+        <a href="https://sirch.cash/" rel="noreferrer" target="_blank" className="link">
+          Support
+        </a>
+        <a href="https://sirch.cash/" rel="noreferrer" target="_blank" className="button">
+          Buy S Cash
+        </a>
+      </div>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 100%;
+  height: 70px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+  padding: 0 30px;
+  position: fixed;
+  top: 0;
+  left: 0;
+
+  .logo {
+    width: 40px;
+    height: 40px;
+    display: flex;
+    border-radius: 10px;
+    align-items: center;
+    justify-content: center;
+    z-index: 2;
+
+    img {
+      width: 60%;
+    }
+  }
+
+  .navigation {
+    width: auto;
+    height: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+
+    a {
+      text-decoration: none;
+      margin: 0 0 0 30px;
+    }
+
+    a.link {
+      color: var(--white);
+    }
+
+    a.button {
+      padding: 10px 30px;
+      color: var(--black);
+      border-radius: 5px;
+      background: var(--white);
+    }
+  }
+`;
+
+export default Nav;
